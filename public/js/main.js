28544905930a0c138eb94ca5a92d5dda233432b0
unknown <sarfocarbell@gmail.com>
2023-05-31 15:56:01 +0000

    ready for consumption

--- public/js/main.js
+++ public/js/main.js
@@ -83,12 +83,6 @@ observer.observe(sectionHero);
 
 
 
-
-
-
-
-
-
 // ========================= Frequently Asked Questions =======================
 var acc = document.getElementsByClassName("accordion");
 var i;
@@ -110,3 +104,67 @@ for (i = 0; i < acc.length; i++) {
   });
 }
 // End of Frequently Asked Question Section
+
+
+
+
+
+
+
+
+
+
+
+// ======================== Intersection observer for fade in animation ==============
+const header = document.querySelector("nav-bar");
+const sectionOne = document.querySelector(".section-hero");
+
+const faders = document.querySelectorAll(".fade-in");
+const sliders = document.querySelectorAll(".slide-in");
+
+const sectionOneOptions = {
+  rootMargin: "-200px 0px 0px 0px"
+};
+
+// const sectionOneObserver = new IntersectionObserver(function(
+//   entries,  sectionOneObserver ) {
+//   entries.forEach(entry => {
+//     if (!entry.isIntersecting) {
+//       navBar.classList.add("nav-scrolled");
+//     } else {
+//       navBar.classList.remove("nav-scrolled");
+//     }
+//   });
+// },
+// sectionOneOptions);
+
+// sectionOneObserver.observe(sectionOne);
+
+const appearOptions = {
+  threshold: 0,
+  rootMargin: "0px 0px -250px 0px"
+};
+
+const appearOnScroll = new IntersectionObserver(function(
+  entries,
+  appearOnScroll
+) {
+  entries.forEach(entry => {
+    if (!entry.isIntersecting) {
+      return;
+    } else {
+      entry.target.classList.add("appear");
+      appearOnScroll.unobserve(entry.target);
+    }
+  });
+},
+appearOptions);
+
+faders.forEach(fader => {
+  appearOnScroll.observe(fader);
+});
+
+sliders.forEach(slider => {
+  appearOnScroll.observe(slider);
+}
+);
